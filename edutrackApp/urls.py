600edeb08944510c7e595f0b04938f8164129aeb
mzacoding <nyikogiven74@gmail.com>
2025-04-13 20:12:03 +0200

    add lecture view

--- edutrackApp/urls.py
+++ edutrackApp/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('list/', views.students_list_view, name='list'),
     path('update/<str:pk>/', views.update_student_view, name='update'),
     path('delete/<str:pk>/', views.delete_student_view, name='delete'),
+
+
+
+    path('lecture_view/add/', views.add_lecture_view, name='lecture_view/add'),
+    path('lecture_view/list/', views.lecture_list_view, name='lecture_view/list'),
+    path('lecture_view/update/<str:pk>/', views.update_lecture_view, name='lecture_view/update'),
+    path('lecture_view/delete/<str:pk>/', views.delete_lecture_view, name='lecture_view/delete'),
 ]
--- edutrackApp/views.py
+++ edutrackApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
-from .forms import StudentForm
-from .models import Student
+from .models import *
+from .forms import *
+ 
 
 # Create Add Student view 
 def add_student_view(request):
@@ -35,12 +36,53 @@ def update_student_view(request,pk):
       form=StudentForm(instance=student)
     return render(request, 'update.html',{'form':form})
 
- 
-
-
 
 def delete_student_view(request, pk):
     student=Student.objects.get(StudentNumber=pk)
     if request.method=='POST':
         student.delete()
         return redirect('list')
+    
+
+
+
+
+# Create Add Lecture  view 
+def add_lecture_view(request):
+    if request.method=='POST':
+     form=LecturerForm(request.POST)
+
+     if form.is_valid():
+        form.save()
+        return redirect('lecture_view/list')
+
+    else:
+       form=LecturerForm()
+
+    return render(request,'lecture_view/add.html',{'form':form})
+
+
+def lecture_list_view(request):
+    lecturers=Lecturer.objects.all()
+    return render(request, 'lecture_view/list.html',{'lecturers':lecturers})
+
+
+def update_lecture_view(request,pk):
+    lecturer=Lecturer.objects.get(StaffNumber= pk)
+    if request.method=='POST':
+       form=LecturerForm(request.POST,instance=lecturer)
+
+       if form.is_valid():
+          form.save()
+          return redirect('lecture_view/list')
+
+    else :      
+      form=LecturerForm(instance=lecturer)
+    return render(request, 'lecture_view/update.html',{'form':form})
+
+
+def delete_lecture_view(request, pk):
+    lecture=Lecturer.objects.get(StaffNumber=pk)
+    if request.method=='POST':
+        lecture.delete()
+        return redirect('lecture_view/list')    
